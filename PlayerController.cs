74a6686361ba915619eb55e9b918f11e9ec56bbe
GauravPun <43338153+GauravPun@users.noreply.github.com>
2018-09-17 15:50:58 +0530

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public ParticalManager PM;
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			Instantiate (PM.PlayerDie, transform.position, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
